15ee4757dbc1340345845f478530458a7e8e1cf9
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-03-29 14:32:54 -0400

    Everything works now.

--- src/AdminPanel/Main.java
+++ src/AdminPanel/Main.java
@@ -1,27 +1,24 @@
+package AdminPanel;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-
-        Parent root = (BorderPane)FXMLLoader.load(getClass().getResource("FinalUI.fxml"));
-        primaryStage.setTitle("FFSM Navigator");
+        Parent root = FXMLLoader.load(getClass().getResource("../Resources/AdminUI.fxml"));
+        //primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1174, 722));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
--- src/UserPanel/Main.java
+++ src/UserPanel/Main.java
@@ -1,24 +1,29 @@
-package sample;
+package UserPanel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("AdminUI.fxml"));
-        //primaryStage.setTitle("Hello World");
+
+
+        Parent root = (BorderPane)FXMLLoader.load(getClass().getResource("../Resources/FinalUI.fxml"));
+        primaryStage.setTitle("FFSM Navigator");
         primaryStage.setScene(new Scene(root, 1174, 722));
         primaryStage.show();
-
     }
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
