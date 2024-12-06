cc0e8bc859ccf5a27fb111a80c8b311f08a9946a
Jacob I. Komissar <jik@bu.edu>
2024-12-06 02:02:04 -0500

    Fix access modifiers in AccountManager
    Also, deprecate logIn for an upcoming change.

--- src/main/java/entities/AccountManager.java
+++ src/main/java/entities/AccountManager.java
@@ -12,47 +12,49 @@ public class AccountManager {
 	private Map<String, Account> accounts = new HashMap<>();
 	private boolean loggedIn;
 
-	void updateKey(String newName, String oldName) {
+	public void updateKey(String newName, String oldName) {
 		Account tempAccount = accounts.get(oldName);
 		accounts.remove(oldName);
 		accounts.put(newName, tempAccount);
 	}
 
-	Map<String, Account> getAccounts() {
+	public Map<String, Account> getAccounts() {
 		return accounts;
 	}
 
-	AccessLevel getPermissions(String username) {
+	public AccessLevel getPermissions(String username) {
 		return accounts.get(username).getPermissions();
 	}
 
-	Account getAccount(String username) {
+	public Account getAccount(String username) {
 		return accounts.get(username);
 	}
 
-	Account addAccount(String user, String password, AccessLevel permission) {
+	public Account addAccount(String user, String password, AccessLevel permission) {
 		Account newAccount = new Account(user, password, permission);
 		accounts.put(user, newAccount);
 		return newAccount;
 	}
 
-	void deleteAccount(String user) {
+	public void deleteAccount(String user) {
 		accounts.remove(user);
 	}
 
-	boolean isProfessional() {
+	public boolean isProfessional() {
 		return loggedIn;
 	}
 
-	void logIn() {
+	@Deprecated
+	public void logIn() {
 		this.loggedIn = true;
 	}
 
-	void logOut() {
+	public void logOut() {
 		this.loggedIn = false;
 	}
 
-	boolean isLoggedIn() {
+	@Deprecated
+	public boolean isLoggedIn() {
 		return this.loggedIn;
 	}
 }
